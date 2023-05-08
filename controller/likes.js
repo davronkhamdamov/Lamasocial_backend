48a708a1da48e1f3b483d9496177c8a56bd15694
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-08 19:52:01 +0500

    Fixed bug in getting user id

--- controller/likes.js
+++ controller/likes.js
@@ -20,7 +20,7 @@ const updateLike = async (req, res, next) => {
             }, { returning: true })
             const user_id = await Videos.findOne({ where: { id: req.body.video_id } })
             const user = await User.findOne({ where: { id: req.user_id } })
-            if (user_id !== req.user_id) {
+            if (user_id.userId !== req.user_id) {
                 await user_message.create({
                     title: `${user.username} sizga like bosdi`,
                     userId: user_id.userId,
